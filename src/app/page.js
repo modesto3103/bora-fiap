ac35496a87aa3eae5aa55f6446f30e072199144d
modesto3103 <pedromesquita3103@gmail.com>
2023-08-22 12:09:28 -0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,65 @@
+export default function Home() {
+  return (
+    <nav className="flex px-8 py-4 bg-rose-600">
+
+      <ul className="flex gap-64.6">
+
+        <li>
+          <a href="#">
+
+            <h1>Bora?</h1>
+
+          </a>
+
+        </li>
+
+        <li>
+          <a href="#">
+
+            Salgados
+
+          </a>
+
+        </li>
+
+        <li>
+          <a href="#">
+
+            Padocas
+
+          </a>
+
+        </li>
+
+        <li>
+          <a href="#">
+
+            Hambúrguers
+
+          </a>
+
+        </li>
+
+        <li>
+          <a href="#">
+
+            Podrão
+
+          </a>
+
+        </li>
+
+        <li>
+          <a href="#">
+
+            Café
+
+          </a>
+
+        </li>
+
+      </ul>
+
+    </nav>
+  )
+}
