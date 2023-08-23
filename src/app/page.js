1e10adcf9f084eb606d049836aab30173b649982
modesto3103 <pedromesquita3103@gmail.com>
2023-08-22 22:37:43 -0300

    title

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,55 @@
+import Title from "@/components/Title";
+
 export default function Home() {
   return (
-    <nav className="flex px-8 py-4 bg-rose-600">
+    <>
+      <nav className="flex px-8 py-4 bg-rose-600">
+        <ul className="flex gap-64.6">
+          <li>
+            <a href="#">
+              <h1>Bora?</h1>
+            </a>
+          </li>
 
-      <ul className="flex gap-64.6">
+          <li>
+            <a href="#">Salgados</a>
+          </li>
 
-        <li>
-          <a href="#">
+          <li>
+            <a href="#">Padocas</a>
+          </li>
 
-            <h1>Bora?</h1>
+          <li>
+            <a href="#">Hambúrguers</a>
+          </li>
 
+          <li>
+            <a href="#">Podrão</a>
+          </li>
+
+          <li>
+            <a href="#">Café</a>
+          </li>
+        </ul>
+      </nav>
+
+      <Title>dicas do dia</Title>
+
+      
+        <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
+          <img className="rounded" src="https://place-hold.it/150x220/666" alt="" />
+          <span className="font-bold text-center line-clamp-1">A dica do dia é...</span>
+          <div>
+            <span>dica...</span>
+          </div>
+          <a href="/" className="bg-pink-600 py-2 w-full rounded text-center">
+            mais dicas
           </a>
+        </div>
+      
 
-        </li>
-
-        <li>
-          <a href="#">
-
-            Salgados
-
-          </a>
-
-        </li>
-
-        <li>
-          <a href="#">
-
-            Padocas
-
-          </a>
-
-        </li>
-
-        <li>
-          <a href="#">
-
-            Hambúrguers
-
-          </a>
-
-        </li>
-
-        <li>
-          <a href="#">
-
-            Podrão
-
-          </a>
-
-        </li>
-
-        <li>
-          <a href="#">
-
-            Café
-
-          </a>
-
-        </li>
-
-      </ul>
-
-    </nav>
-  )
+      <Title>lançamentos</Title>
+      <Title>favoritos</Title>
+    </>
+  );
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,5 @@
+export default function Title({children}) {
+    return (
+        <h2 className="text-x1 font-semibold bg-rose-600 border-pink-600 border-1-8 m-2 pl-2">{children}</h2>
+    )
+}
